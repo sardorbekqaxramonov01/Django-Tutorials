013c1d6aedfbcd44ff8224b29e0ff283fbb56409
sardorbekqaxramonov01 <qaxramonovsardorbek03@gmail.com>
2023-07-10 17:45:17 +0300

    done

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0o3m&aibw$+40=&@9tginrsvz+*!l(lk_b2oal3^tlxsv3_jds
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
--- hello_world/views.py
+++ hello_world/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(home):
+    return HttpResponse("""
+    <style>
+        *{
+            padding:0;
+            margin:0;
+            box-sizing:border-box;
+        }
+        body{
+            display:flex;
+            justify-content:center;
+            text-align:center;
+        }
+        h1{
+            color:blue;
+        }
+    </style>
+
+    <h1>Hello world</h1>"""
+)
+
+def about(about):
+    return HttpResponse("""
+    <style>
+        *{
+            padding:0;
+            margin:0;
+            box-sizing:border-box;
+        }
+        body{
+            display:flex;
+            justify-content:center;
+            text-align:center;
+        }
+        h1{
+            color:blue;
+        }
+    </style>
+
+    <h1>Salom dunyo</h1>"""
+)
