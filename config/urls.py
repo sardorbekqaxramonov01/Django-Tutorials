505f318a7155d9650d807310e3f16be5d0b32b9e
sardorbekqaxramonov01 <qaxramonovsardorbek03@gmail.com>
2023-07-12 20:11:03 +0500

    done

--- config/urls.py
+++ config/urls.py
@@ -6,8 +6,4 @@ urlpatterns = [
     # path("", include("hello_world.urls")),
    
     path('',include('my_blog.urls')),
-    path("home/", include("my_blog.urls")),
-    path("about/", include("my_blog.urls")),
-    path("project/", include("my_blog.urls")),
-    path("contact/", include("my_blog.urls")),
 ]
--- my_blog/views.py
+++ my_blog/views.py
@@ -1,126 +1,18 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
-def home(request):
-    return HttpResponse("""
-    <style>
-        *{
-            padding:0;
-            margin:0;
-            box-sizing:border-box;
-        }
-        body{
-            display:flex;
-            justify-content:center;
-            text-align:center;
-        }
-        a{
-            color:blue;
-            margin:10px;
-        }
-    </style>
+def home(req):
+    return render(req,"index.html")
 
-    <a href="/about">About</a>
-    <a href="/project">Project</a>
-    <a href="/my_blog">My_blog</a>
-    <a href="/contact">Contact</a>
-    """
-)
+def about(req):
+    return render(req,"index.html")
 
-def about(request):
-    return HttpResponse("""
-    <style>
-        *{
-            padding:0;
-            margin:0;
-            box-sizing:border-box;
-        }
-        body{
-            display:flex;
-            justify-content:center;
-            text-align:center;
-        }
-        a{
-            color:blue;
-        }
-    </style>
+def project(req):
+    return render(req,"index.html")
 
-    <a href="/home">Home</a>
-    <a href="/project">Project</a>
-    <a href="/my_blog">My_blog</a>
-    <a href="/contact">Contact</a>"""
-)
+def my_blog(req):
+    return render(req,"index.html")
 
-def project(request):
-    return HttpResponse("""
-    <style>
-        *{
-            padding:0;
-            margin:0;
-            box-sizing:border-box;
-        }
-        body{
-            display:flex;
-            justify-content:center;
-            text-align:center;
-        }
-        a{
-            color:blue;
-            margin:10px;
-        }
-    </style>
-    <a href="/home">Home</a>
-    <a href="/about">About</a>
-    <a href="/my_blog">My_blog</a>
-    <a href="/contact">Contact</a>"""
-)
-
-def my_blog(request):
-    return HttpResponse("""
-    <style>
-        *{
-            padding:0;
-            margin:0;
-            box-sizing:border-box;
-        }
-        body{
-            display:flex;
-            justify-content:center;
-            text-align:center;
-        }
-        a{
-            color:blue;
-            margin:10px;
-        }
-    </style>
-                        
-    <a href="/home">Home</a>
-    <a href="/about">About</a>
-    <a href="/project">Project</a>
-    <a href="/contact">Contact</a>"""
-)
-
-def contact(request):
-    return HttpResponse("""
-    <style>
-        *{
-            padding:0;
-            margin:0;
-            box-sizing:border-box;
-        }
-        body{
-            display:flex;
-            justify-content:center;
-            text-align:center;
-        }
-        a{
-            color:blue;
-            margin:10px;
-        }
-    </style>
-
-    <a href="/home">Home</a>
-    <a href="/about">About</a>
-    <a href="/my_blog">My_blog</a>
-    <a href="/project">Project</a>"""
-)
+def contact(req):
+    return render(req,"index.html")
