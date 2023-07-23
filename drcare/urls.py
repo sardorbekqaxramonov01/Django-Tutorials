eb49d870c9060891cd9869074f61f6d4bc75a3ed
sardorbekqaxramonov01 <qaxramonovsardorbek03@gmail.com>
2023-07-23 21:51:48 +0500

    'done'

--- drcare/urls.py
+++ drcare/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("about/", views.about, name="about"),
+    path("doctor/", views.doctor, name="doctor"),
+    path("department/", views.department, name="department"),
+    path("pricing/", views.pricing, name="pricing"),
+    path("blog/", views.blog, name="blog"),
+    path("contact/", views.contact, name="contact"),
 ]
--- drcare/views.py
+++ drcare/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 
+def home(req):
+    return render(req,"base.html",)
+
+def about(req):
+    return render(req,"pages/about.html",)
+
+def appointment(req):
+    return render(req,"pages/appointment.html",)
+
+def blog(req):
+    return render(req,"pages/blog.html",)
+
+def contact(req):
+    return render(req,"pages/contact.html",)
+
+def department(req):
+    return render(req,"pages/department.html",)
+
+def doctor(req):
+    return render(req,"pages/doctor.html",)
+
+def pricing(req):
+    return render(req,"pages/pricing.html",)
+
