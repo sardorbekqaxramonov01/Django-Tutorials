0f77027ba46f98904c66915f74837844ca571dc4
sardorbekqaxramonov01 <qaxramonovsardorbek03@gmail.com>
2023-07-27 20:03:57 +0500

    done

--- Barer_shop/models.py
+++ Barer_shop/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 
+class Price(models.Model):
+     photo = models.ImageField(upload_to="media")
+     starting = models.FloatField(max_length=6)
+     Haircut = models.FloatField(max_length=6)
+     Beard_Trim = models.FloatField(max_length=6)
+     Razor_Cut = models.FloatField(max_length=6)
+     Shaves = models.FloatField(max_length=6)
+     Styling_Color = models.FloatField(max_length=6)
+
 class Contact(models.Model):
     open_time = models.CharField(max_length=255)
     email = models.EmailField(max_length=255)
@@ -9,3 +18,4 @@ class Contact(models.Model):
 class Branche(models.Model):
     name = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
+
--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -8,4 +10,4 @@ urlpatterns = [
     # path('',include('my_blog.urls')),
     path("",include("Barer_shop.urls")),
     # path("", include("drcare.urls"))
-]
+]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
